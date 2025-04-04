fc1173ac25dc23682fec89aadb6012f94ff53c9b
arunes <ali@arunes.com>
2025-04-04 11:59:39 -0500

    feat: adding comments liked counter

--- counters/lib/comment_liked_by_others.ex
+++ counters/lib/comment_liked_by_others.ex
@@ -5,7 +5,7 @@ defmodule Moc.Counters.CommentLikedByOthers do
   @spec count(Type.Input.t(), fun()) :: list(Type.counter_result())
   def count(%Type.Input{comments: comments}, _get_data) do
     comments
-    |> Enum.filter(&(&1.comment_type == "text" and &1.liked_by != ""))
+    |> Enum.filter(&(&1.comment_type == "text" && &1.liked_by != ""))
     |> Enum.map(fn cmt ->
       %{contributor_id: cmt.created_by_id, count: count_likes(cmt.liked_by, cmt.created_by_id)}
     end)
