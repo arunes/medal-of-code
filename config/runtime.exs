37d85e406dd185aa4145e649c171d73f2b4e7569
arunes <ali@arunes.com>
2025-05-09 12:56:28 -0500

    feat: removing requirement of secret key

--- config/runtime.exs
+++ config/runtime.exs
@@ -24,16 +24,9 @@ if config_env() == :prod do
   config :moc, Moc.Repo, pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+      "HmFb+IKzt//ysAbQt1m7a+E5g1wycIulBPOBkIZ25d86XIAebfDitkLS/xO33ubN"
 
   host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
